3a72228ba97ef9861e4703b09ebdad4d3b417395
Etti Kehinde <116558740+GeminiiTech@users.noreply.github.com>
2025-04-04 20:49:19 +0100

    removed auth from album-detail

--- auth_service/serializer.py
+++ auth_service/serializer.py
@@ -7,7 +7,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.urls import reverse
 from django.utils.encoding import force_str
-from .email_service import send_email
+# from .email_service import send_email
 
 
 class UserSerializer(serializers.ModelSerializer):
